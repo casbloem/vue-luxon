f1e704ef8de7e5af88ef32aaa95768e112565dbb
Cas Bloem <casbloem@outlook.com>
2018-03-17 03:44:41 +0100

    test

--- test.js
+++ test.js
@@ -2,15 +2,18 @@ const chalk = require('chalk');
 
 module.exports = {
     initialStarter() {
-        console.log(chalk.green(chalk.blue.underline.bold('js-tester 0.0.1') + ' by cblm dev\n\n'));
-        console.log(chalk.blue.bgBlue.bold('starting setup...\n'));
-        console.log(chalk.blue.bgWhite.bold('importing package(s)\n'));
+        const log = (str) => {
+            console.log(chalk.blue.bgWhite(str+'\n'));
+        }
+        log(chalk.green(chalk.blue.underline.bold('js-tester 0.0.1') + ' by cblm dev\n'));
+        log(chalk.blue.bgBlue.bold('starting setup...'));
+        log(chalk.blue.bgWhite.bold('importing package(s)\n'));
         // import package
         const vueLuxon = require('./vue-luxon.js').vueluxon();
 
 
 
-        console.log(chalk.blue.bgBlue.bold('Setup completed, starting tests now...\n\n'));
+        console.log(chalk.blue.bgBlue.bold('Setup completed, starting tests now...'));
 
         console.log(
             vueLuxon('2012')
