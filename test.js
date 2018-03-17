d89659b1b50a5ab718762b88ecfc97c53783c3a6
Cas Bloem <casbloem@outlook.com>
2018-03-17 04:13:02 +0100

    updating... hold on!

--- test.js
+++ test.js
@@ -1,55 +1,51 @@
 module.exports = {
-    initialStarter() {
-        const log = (str) => {
-            console.log(str);
-        }
-        log('js-tester 0.0.1 by cblm dev\n');
-        log('starting setup...');
-        log('importing package(s)\n');
-        // import package
-        const vueLuxon = require('./vue-luxon.js').vueluxon();
+  initialStarter() {
+    const log = str => {
+      console.log(str);
+    };
+    log("js-tester 0.0.1 by cblm dev\n");
+    log("starting setup...");
+    log("importing package(s)\n");
+    const vueLuxon = require("./vue-luxon.js").vueluxon();
 
-        
+    log("setup completed, starting tests now...");
 
+    const tests = [
+      {
+        input: vueLuxon("2012", {
+          serverZone: "UTC",
+          serverFormat: "yyyy",
+          clientZone: "UTC",
+          clientFormat: "yyyy"
+        }),
+        expected_output: "2012"
+      },
+      {
+        input: vueLuxon("2011-04-02", {
+          serverZone: "UTC",
+          serverFormat: "yyyy-MM-dd",
+          clientZone: "en-US",
+          clientFormat: "yy-dd-MM"
+        }),
+        expected_output: "11-02-04"
+      }
+    ];
 
+    log("---------------------------------------");
 
-        log('setup completed, starting tests now...');
-
-        const tests = [
-            {
-                input: vueLuxon('2012', {
-                    serverZone: "UTC",
-                    serverFormat: "yyyy",
-                    clientZone: "UTC",
-                    clientFormat: "yyyy", }),
-                expected_output: '2012',
-            },
-            {
-                input: vueLuxon('2011-04-02', {
-                    serverZone: "UTC",
-                    serverFormat: "yyyy-MM-dd",
-                    clientZone: "en-US",
-                    clientFormat: "yy-dd-MM", }),
-                expected_output: '11-02-04',
-            }
-        ]
-
-        log('---------------------------------------');
-    
-        var fails = 0;
-        for(var i = 0; i < tests.length; i++) {
-            let test = tests[i];
-            log('> expects: ' + test.expected_output);
-            log('> gets: ' + test.input);
-            if (test.input == test.expected_output) log('TEST '+ i +' SUCCESS');
-            else { fails++; log('TEST '+ i +' FAILED'); }
-            log('---------------------------------------');
-        }
-    
-
-        
-
-
-        return 'Testing completed! (' + i + ' tests, ' + fails + ' failed)';
+    var fails = 0;
+    for (var i = 0; i < tests.length; i++) {
+      let test = tests[i];
+      log("> expects: " + test.expected_output);
+      log("> gets: " + test.input);
+      if (test.input == test.expected_output) log("TEST " + i + " SUCCESS");
+      else {
+        fails++;
+        log("TEST " + i + " FAILED");
+      }
+      log("---------------------------------------");
     }
-}
+
+    return "Testing completed! (" + i + " tests, " + fails + " failed)";
+  }
+};
